0fb0c818f22a9b8c42d0d0e0c73b2f5b5e1228a6
sssloba <slobodan.stojakov@gmail.com>
2020-04-28 22:54:26 +0200

    adding-form-with-global-key

--- lib/messageCompose.dart
+++ lib/messageCompose.dart
@@ -11,6 +11,7 @@ class _MessageComposeState extends State<MessageCompose> {
   String subject = "";
   String body = "";
 
+  final key = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -19,73 +20,71 @@ class _MessageComposeState extends State<MessageCompose> {
           title: Text("Compose new message"),
         ),
         body: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              ListTile(
-                title: TextField(
-                  onChanged: (value) {
-                    to = value;
-                  },
-                  decoration: InputDecoration(
-                      labelText: 'TO',
-                      labelStyle: TextStyle(fontWeight: FontWeight.bold)),
+          child: Form(
+            key: key,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                ListTile(
+                  title: TextFormField(
+                    onSaved: (value) => to = value,
+                    decoration: InputDecoration(
+                        labelText: 'TO',
+                        labelStyle: TextStyle(fontWeight: FontWeight.bold)),
+                  ),
                 ),
-              ),
-              ListTile(
-                title: TextField(
-                  onChanged: (value) {
-                    subject = value;
-                  },
-                  decoration: InputDecoration(
-                      labelText: 'SUBJECT',
-                      labelStyle: TextStyle(fontWeight: FontWeight.bold)),
+                ListTile(
+                  title: TextFormField(
+                    onSaved: (value) => subject = value,
+                    decoration: InputDecoration(
+                        labelText: 'SUBJECT',
+                        labelStyle: TextStyle(fontWeight: FontWeight.bold)),
+                  ),
                 ),
-              ),
-              Divider(),
-              ListTile(
-                title: TextField(
-                  onChanged: (value) {
-                    body =value;
-                  },
-                  decoration: InputDecoration(
-                      labelText: 'BODY',
-                      labelStyle: TextStyle(fontWeight: FontWeight.bold)),
-                  maxLines: 9,
+                Divider(),
+                ListTile(
+                  title: TextFormField(
+                    onSaved: (value) => body = value,
+                    decoration: InputDecoration(
+                        labelText: 'BODY',
+                        labelStyle: TextStyle(fontWeight: FontWeight.bold)),
+                    maxLines: 9,
+                  ),
                 ),
-              ),
-              ListTile(
-                title: RaisedButton(
-                  child: Text('SEND'),
-                  onPressed: () {
-                    Message message = Message(subject, body);
+                ListTile(
+                    title: RaisedButton(
+                        child: Text('SEND'),
+                        onPressed: () {
+                          this.key.currentState.save();
+                          
+                          Message message = Message(subject, body);
+                                                    
+                          Navigator.pop(context, message);
+                          // print(to);
+                          // print(subject);
+                          // print(body);
+                        })),
 
-                    Navigator.pop(context, message);
-                    // print(to);
-                    // print(subject);
-                    // print(body);
-                })
-              ),
-
-              // Text("Compose new message",
-              //   style: Theme.of(context).textTheme.title),
-              // Row(
-              //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              //   children: <Widget>[
-              //     RaisedButton(
-              //       child: Text("Love"),
-              //       onPressed: () {
-              //         Navigator.pop(context, "Love");
-              //       },
-              //     ),
-              //     RaisedButton(
-              //       child: Text("Hate"),
-              //       onPressed: () {
-              //         Navigator.pop(context, "Hate");
-              //       },
-              //     ),
-              //],),
-            ],
+                // Text("Compose new message",
+                //   style: Theme.of(context).textTheme.title),
+                // Row(
+                //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                //   children: <Widget>[
+                //     RaisedButton(
+                //       child: Text("Love"),
+                //       onPressed: () {
+                //         Navigator.pop(context, "Love");
+                //       },
+                //     ),
+                //     RaisedButton(
+                //       child: Text("Hate"),
+                //       onPressed: () {
+                //         Navigator.pop(context, "Hate");
+                //       },
+                //     ),
+                //],),
+              ],
+            ),
           ),
         ));
   }
