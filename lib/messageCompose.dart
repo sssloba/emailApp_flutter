edb651fbccc103018206598346c12ab4f8c627cf
sssloba <slobodan.stojakov@gmail.com>
2020-04-28 23:13:46 +0200

    form-validation

--- lib/messageCompose.dart
+++ lib/messageCompose.dart
@@ -27,6 +27,9 @@ class _MessageComposeState extends State<MessageCompose> {
               children: <Widget>[
                 ListTile(
                   title: TextFormField(
+                    validator: (value) => !value.contains('@')
+                        ? "`TO` field must be a valid email"
+                        : null,
                     onSaved: (value) => to = value,
                     decoration: InputDecoration(
                         labelText: 'TO',
@@ -35,6 +38,15 @@ class _MessageComposeState extends State<MessageCompose> {
                 ),
                 ListTile(
                   title: TextFormField(
+                    validator: (value) {
+                      int len = value.length;
+                      if (len == 0) {
+                        return "`SUBJECT` cannot be empty";
+                      } else if (len <4) {
+                        return "`SUBJECT` must be longer than 4 characters";
+                      }
+                      return null;
+                    },
                     onSaved: (value) => subject = value,
                     decoration: InputDecoration(
                         labelText: 'SUBJECT',
@@ -55,11 +67,14 @@ class _MessageComposeState extends State<MessageCompose> {
                     title: RaisedButton(
                         child: Text('SEND'),
                         onPressed: () {
-                          this.key.currentState.save();
-                          
-                          Message message = Message(subject, body);
-                                                    
-                          Navigator.pop(context, message);
+                          if (this.key.currentState.validate()) {
+                            this.key.currentState.save();
+
+                            Message message = Message(subject, body);
+
+                            Navigator.pop(context, message);
+                          }
+
                           // print(to);
                           // print(subject);
                           // print(body);
